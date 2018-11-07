bae3efb85698f675889ed0bfce2b4e305dc44843
jacky.leung <jacky.leung@oocl.com>
2018-11-07 10:37:06 +0800

    complete section-2

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,23 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var key = '';
+	var count;
+	var res = new Array();
+	var col = Object.values(collection);
+	var str = '';
+	var obj;
+
+	for (var i = 0; i < col.length; i++) {
+		if (col[i] == key) {
+			continue;
+		}
+		key = col[i];
+		count = col.filter(item => item == key).length;
+		
+		str = '{ "key": "' + key + '", "count":' + count + ' }';
+		res.push(JSON.parse(str));
+	}
+	console.log("Credit to: Ivan Lin")
+  return res;
 }
--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,35 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var key = '';
+	var count;
+	var res = new Array();
+	var col = Object.values(collection);
+	var new_col = new Array();
+	var str = '';
+	var tmp;
+
+	for (var i = 0; i < col.length; i++) {
+		tmp = col[i].split('-');
+		if (tmp.length > 1) {
+			for (var j = 0; j < tmp[1]; j++) {
+				new_col.push(tmp[0]);
+			}
+			continue;
+		}
+
+		new_col.push(col[i]);
+	}
+
+	for (var i = 0; i < new_col.length; i++) {
+		if (new_col[i] == key) {
+			continue;
+		}
+		key = new_col[i];
+		count = new_col.filter(item => item == key).length;
+		
+		str = '{ "key": "' + key + '", "count":' + count + ' }';
+		res.push(JSON.parse(str));
+	}
+  return res;
 }
