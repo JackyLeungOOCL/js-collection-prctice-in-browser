5386c359579a55e722370cc996de8e66f8d36a2f
jacky.leung <jacky.leung@oocl.com>
2018-11-21 11:48:27 +0800

    complete pre-training w/o loop

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,9 +1,5 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-	var colA = new Array();
-	for (var i = 0; i < collectionA.length; i++) {
-		colA = colA.concat(collectionA[i].key);
-	}
-  return colA.filter(item => objectB.value.includes(item));
+	return collectionA.map(a => a.key).filter(a => objectB.value.includes(a));
 }
